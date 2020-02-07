2bb68e125100611d7146ea9f167cdefaea855910
gimoz71 <gm3871@gmail.com>
2020-02-07 12:17:06 +0100

    creazione componente utenti

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { UtentiComponent } from './components/utenti/utenti.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'utenti', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'utenti', component: UtentiComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,14 @@ import { BvinoLibModule, SessionService, BVAuthorizationService, ConstantsServic
 import { FormsModule } from '@angular/forms';
 import { AppSessionService } from './services/appSession.service';
 import { CookieService } from 'ngx-cookie-service';
+import { UtentiComponent } from './components/utenti/utenti.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
+    UtentiComponent,
   ],
   imports: [
     BrowserModule,
